f29fa64f72936a6f7bc5de37ed64ef4512f0cb5d
boris-RIVERA <147168425+boris-RIVERA@users.noreply.github.com>
2023-10-16 11:13:38 -0400

    agregando

---
+++ WebApplication7/Models/Universidad.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.Models
+{
+    public class Universidad
+    {
+        [Key]
+        public int iduniversidad { get; set; }
+        public string nombre { get; set; }
+    }
+}
